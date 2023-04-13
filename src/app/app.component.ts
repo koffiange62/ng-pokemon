65e13025b52899dca184c36ecd9eb8af1c0ac723
koffiange <koffiange62@gmail.com>
2023-04-13 10:44:23 -0400

    Change Log : Section 5 - les composantes web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Pokemon} from "./Pokemon";
+import {POKEMONS} from "./mock-pokmon-list";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-pokemon';
+export class AppComponent implements OnInit{
+  pokemonList : Pokemon [];
+
+  ngOnInit(): void {
+    this.pokemonList = POKEMONS;
+    console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[0]);
+  }
+
+  selectPokemon(pokemon : Pokemon) : void {
+    console.log(`Vous avez selctionné ${pokemon.name}`);
+  }
+
 }
