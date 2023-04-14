40df4e296129d951fa1d5e2ee475128eed4796af
koffiange <koffiange62@gmail.com>
2023-04-14 14:09:39 -0400

    Change Log : Section 6 - ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,20 @@ import {POKEMONS} from "./mock-pokmon-list";
 })
 export class AppComponent implements OnInit{
   pokemonList : Pokemon [];
+  selectedPokemon : Pokemon|undefined;
 
   ngOnInit(): void {
     this.pokemonList = POKEMONS;
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemon : Pokemon) : void {
-    console.log(`Vous avez selctionné ${pokemon.name}`);
+  selectPokemon(pokemonId : string) : void {
+    this.selectedPokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    if (this.selectedPokemon){
+      console.log(`Pokemon selectioné : ${this.selectedPokemon.name}`)
+    } else {
+      console.log(`Aucun pokémon trouvé pour l'id : ${pokemonId}`)
+    }
   }
 
 }
