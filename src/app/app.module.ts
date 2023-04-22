c2d7de29309718aacc0f1303e613813bb5eb0df0
koffiange <koffiange62@gmail.com>
2023-04-22 12:30:02 -0400

    Change Log : Section 10 : Les module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
-import { AppRoutingModule } from './app-routing.module';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {PokemonModule} from "./pokemon/pokemon.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
-    PageNotFoundComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    PokemonModule,
     AppRoutingModule
   ],
   providers: [],
